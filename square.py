0f565a535ea8f059db9fa4e32944b5a959939259
root <Emdinga>
2023-10-12 14:39:54 -0700

    fix my code

--- square.py
+++ square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+""" define square """
 
 class square():
-    
+""" square class """    
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+	""" square class initialization"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,12 +17,19 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+	""" permit of a squre """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+	""" string representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
+    """ Create Square instance """
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
 
     s = square(width=12, height=9)
     print(s)
